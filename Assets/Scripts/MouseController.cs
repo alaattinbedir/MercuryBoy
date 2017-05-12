f49a73548aec3cebf0483567afcf55201f8d53a4
Alaattin Bedir <alaattin.bedir@etiya.com>
2017-05-12 10:37:03 +0300

    initial project commit

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
+	public Rigidbody2D rb;
+	public Transform groundCheckTransform;
+
+	private bool grounded;
+
+	public LayerMask groundCheckLayerMask;
+
+	Animator animator;
+	public ParticleSystem jetpack;
+
+	private bool dead = false;
+
+	private uint coins = 0;
+
+	public Texture2D coinIconTexture;
+
+	public AudioClip coinCollectSound;
+
+	public AudioSource jetpackAudio;
+
+	public AudioSource footstepsAudio;
+
+	public ParallaxScroll parallax;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () 
+	{
+		bool jetpackActive = Input.GetButton("Fire1");
+		jetpackActive = jetpackActive && !dead;
+		if (jetpackActive)
+		{
+			rb.AddForce(new Vector2(0, jetpackForce));
+		}
+
+		if (!dead) {
+			Vector2 newVelocity = rb.velocity;
+			newVelocity.x = forwardMovementSpeed;
+			rb.velocity = newVelocity;
+		}
+
+		UpdateGroundedStatus();
+
+		AdjustJetpack(jetpackActive);
+
+		AdjustFootstepsAndJetpackSound(jetpackActive);
+
+		parallax.offset = transform.position.x;
+	}
+
+	void DisplayCoinsCount()
+	{
+		Rect coinIconRect = new Rect(10, 10, 32, 32);
+		GUI.DrawTexture(coinIconRect, coinIconTexture);                         
+
+		GUIStyle style = new GUIStyle();
+		style.fontSize = 30;
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.yellow;
+
+		Rect labelRect = new Rect(coinIconRect.xMax, coinIconRect.y, 60, 32);
+		GUI.Label(labelRect, coins.ToString(), style);
+	}
+
+	void OnGUI()
+	{
+		DisplayCoinsCount();
+
+		DisplayRestartButton();
+	}
+
+	void AdjustJetpack (bool jetpackActive)
+	{
+		jetpack.enableEmission = !grounded;
+		jetpack.emissionRate = jetpackActive ? 300.0f : 75.0f; 
+	}
+
+	void AdjustFootstepsAndJetpackSound(bool jetpackActive)    
+	{
+		footstepsAudio.enabled = !dead && grounded;
+
+		jetpackAudio.enabled =  !dead && !grounded;
+		jetpackAudio.volume = jetpackActive ? 1.0f : 0.5f;        
+	}
+
+	void CollectCoin(Collider2D coinCollider)
+	{
+		coins++;
+
+		Destroy(coinCollider.gameObject);
+
+		AudioSource.PlayClipAtPoint(coinCollectSound, transform.position);
+	}
+
+	void UpdateGroundedStatus()
+	{
+		//1
+		grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+
+		//2
+		animator.SetBool("grounded", grounded);
+	}
+
+	void DisplayRestartButton()
+	{
+		if (dead && grounded)
+		{
+			Rect buttonRect = new Rect(Screen.width * 0.35f, Screen.height * 0.45f, Screen.width * 0.30f, Screen.height * 0.1f);
+			if (GUI.Button(buttonRect, "Tap to restart!"))
+			{
+				Application.LoadLevel (Application.loadedLevelName);
+			};
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.CompareTag("coin"))
+			CollectCoin(collider);
+		else
+			HitByLaser(collider);
+	}
+
+	void HitByLaser(Collider2D laserCollider)
+	{
+		if (!dead)
+			laserCollider.gameObject.GetComponent<AudioSource>().Play();
+		
+		dead = true;
+		animator.SetBool("dead", true);
+	}
+}
